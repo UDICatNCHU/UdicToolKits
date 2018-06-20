6e0b9906a6185b812b6cca7632bfd89a2cff0409
UDICatNCHU <yfan@nchu.edu.tw>
2018-06-20 15:27:14 +0800

    test4

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name = 'udicOpenData',
-    packages=['udicOpenData'],
-    package_dir={'udicOpenData':'udicOpenData'},
-    package_data={'udicOpenData':['dictionary/*', 'stopwords/*', 'scripts/dump2es.py']},
-    version = '1.3',
-    description = 'udic dictionary, stopwords module',
-    author = ['davidtnfsh'],
-    author_email = 'davidtnfsh@gmail.com',
+    name = 'UdicToolKits',
+    packages=['TextPreprocessing'],
+    version = '1.0',
+    description = 'udic dictionary, stopwords, TextPreprocessing module',
+    author = ['yaochungfan'],
+    author_email = 'yfan@nchu.edu.tw',
     url = 'https://github.com/UDICatNCHU/Open-Training-Data',
     download_url = '',
-    keywords = ['udicOpenData', 'jieba', 'dictionary', 'stopwords'],
+    keywords = ['TextPreprocessing', 'jieba', 'dictionary', 'stopwords'],
     classifiers = [],
     license='GPL3.0',
-    install_requires=[
-        'jieba'
-    ],
     zip_safe=True,
-    scripts=['udicOpenData/scripts/dump2es.py']
 )
