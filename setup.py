97d3c007a84c7ce0cb5edb78a28d38cade1f7a51
UDICatNCHU <yfan@nchu.edu.tw>
2018-06-21 09:20:50 +0800

    fix_bugs

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
+import setuptools
 from distutils.core import setup
 
 setup(
     name = 'UdicToolKits',
-    packages=['UdicToolKits'],
-    package_dir={'UdicToolKits'},
-    package_data={'UdicToolKits':['TextPreprocessing/*']},
-    version = '1.0',
+    #packages=['UdicToolKits'],
+    #package_dir={'UdicToolKits':'TextPreprocessing'},
+    #package_data={'UdicToolKits':['TextPreprocessing/*']},
+    packages = setuptools.find_packages(),
+    version = '1.04',
     description = 'udic dictionary, stopwords, TextPreprocessing module',
     author = ['yaochungfan'],
     author_email = 'yfan@nchu.edu.tw',
